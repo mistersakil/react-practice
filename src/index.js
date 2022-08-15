c61b9fdf05a293928faf4e7ebc28dc3a4c156c6e
Sakil Mahmud <sakil.diu.cse@gmail.com>
2022-08-15 21:58:49 +0600

    created a functional component one

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import Clock from './components/Clock';
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import Clock from "./components/Clock";
+import One from "./components/One";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
     <Clock locale="bn-BD" />
-    <Clock locale="en-UK" />
-    <Clock locale="en-IN" />
-    <Clock locale="ko-KR" />
-    <Clock locale="de-DE" />
-    <Clock locale="ja-JP" />
+    <One />
   </>
 );
