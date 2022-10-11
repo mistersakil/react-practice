3e4c4ba1498baafb18d60df52432509eb0493074
mistersakil <sakil.diu.cse@gmail.com>
2022-10-11 19:34:32 +0600

    working on useCallback hook

--- src/components/UseCallbackHook.js
+++ src/components/UseCallbackHook.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useMemo } from "react";
+import React, { useState, useCallback, useMemo, useEffect } from "react";
 import CountDetails from "./CountDetails";
 import ButtonCount from "./ButtonCount";
 import Title from "./Title";
@@ -6,19 +6,25 @@ import Title from "./Title";
 const UseCallbackHook = () => {
   const [countOne, setCountOne] = useState(1);
   const [countFive, setCountFive] = useState(5);
+
+  /** useCallback */
   const incrementByOne = useCallback(() => {
-    // setCountOne(countOne + 1);
     setCountOne((previous) => previous + 1);
   }, []);
+  /** useCallback */
   const incrementByFive = useCallback(() => {
     setCountFive((previous) => previous + 5);
   }, []);
+  /** useEffect */
+  function numberType() {
+    return countOne % 2 == 0 ? "even" : "odd";
+  }
   return (
     <div>
       <Title />
+      Number type: {numberType()}
       <CountDetails value={countOne} title="Counter 1" />
       <ButtonCount increment={incrementByOne}>Increment by 1</ButtonCount>
-
       <CountDetails value={countFive} title="Counter 2" />
       <ButtonCount increment={incrementByFive}>Increment by 5</ButtonCount>
     </div>
